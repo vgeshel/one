7052ed43ecfdf9557b468fc3cdffa7bec9cdf32f
Brenton Ashworth <brenton@thinkrelevance.com>
2011-12-13 23:01:57 -0500

    All REPL connections from remote machines.

--- src/cljs/start/core.cljs
+++ src/cljs/start/core.cljs
@@ -1,10 +1,15 @@
 (ns ^{:doc "Contains the entry point for the ClojureScript sample application."}
   start.core
-  (:require [clojure.browser.repl :as repl]
+  (:require [goog.uri.utils :as uri]
+            [clojure.browser.repl :as repl]
             [library.dispatch :as dispatch]
             [start.view :as view]))
 
 ;; TODO: Add marginalia comment to explain what :export is for.
+
+(defn- domain
+  []
+  (uri/getDomain (.toString window.location ())))
 
 (defn ^:export repl
   "Connects to a ClojureScript REPL running on localhost port 9000.
@@ -13,7 +18,7 @@
   browser for evaluation. This function should be called from a script
   in the development host HTML page."
   []
-  (repl/connect "http://localhost:9000/repl"))
+  (repl/connect (str "http://" (domain) ":9000/repl")))
 
 (defn ^:export start
   "Start the application by firing a :form event which will cause the
